4011380aeafe9ca9911cffd6efcfcf97816518b0
Brian Reynolds <brian@growratio.com>
2020-03-12 10:57:43 -0400

    base react native app

---
+++ index.js
@@ -0,0 +1,10 @@
+/**
+ * @format
+ */
+
+import {AppRegistry} from 'react-native';
+// import App from './App';
+import ServerStats from "./src/ServerStats";
+import {name as appName} from './app.json';
+
+AppRegistry.registerComponent(appName, () => ServerStats);
---
+++ src/ServerStats.tsx
@@ -0,0 +1,8 @@
+
+const logo = require('images/BrianTV.png');
+import React from 'react';
+
+
+export default function ServerStats() {
+  return null;
+}
