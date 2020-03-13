dc27377f560196dd76c7fca3efd24cee033759a4
Brian Reynolds <brian@growratio.com>
2020-03-13 12:46:47 -0400

    Added a gauge display component with responsive sizing

---
+++ src/components/MemUsage.tsx
@@ -0,0 +1,16 @@
+import Gauge from "components/Gauge";
+import React from "react";
+
+
+export default class MemUsage extends React.Component {
+  render() {
+    return (
+      <Gauge
+        percentage={this.props.memUsed}
+        label="Mem. Usage"
+        valueLabel={`${Math.round(this.props.memUsed)}%`}
+        width="30%"
+      />
+    );
+  }
+}
