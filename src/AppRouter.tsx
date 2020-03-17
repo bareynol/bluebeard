466b3a219371d4d434d38f29c4db58ab0e39c471
Brian Reynolds <brian@growratio.com>
2020-03-17 13:18:13 -0400

    added logos to the sidebar

--- src/AppRouter.tsx
+++ src/AppRouter.tsx
@@ -8,8 +8,21 @@ import SideBar from 'components/SideBar';
 import Plex from 'pages/Plex';
 import Sonarr from 'pages/Sonarr';
 import Ombi from 'pages/Ombi';
+import { Thumbnail } from 'native-base';
+
+const appLogo = require('images/BrianTV.png');
+const plexLogo = require('images/plex_logo.png');
+const sonarrLogo = require('images/sonarr_logo.png');
+const ombiLogo = require('images/ombi_logo.png');
 
 const Drawer = createDrawerNavigator();
+
+const SCREENS = [
+  {name: "Dashboard", component: ServerStats, icon: appLogo},
+  {name: "Plex", component: Plex, icon: plexLogo},
+  {name: "Sonarr", component: Sonarr, icon: sonarrLogo},
+  {name: "Ombi", component: Ombi, icon: ombiLogo},
+]
 
 export default function AppRouter() {
   return (
@@ -20,12 +33,35 @@ export default function AppRouter() {
           sceneContainerStyle={{backgroundColor: 'transparent'}}
           drawerContent={props => <SideBar {...props} />}
         >
-          <Drawer.Screen name="Dashboard" component={ServerStats} />
-          <Drawer.Screen name="Plex" component={Plex} />
-          <Drawer.Screen name="Sonarr" component={Sonarr} />
-          <Drawer.Screen name="Ombi" component={Ombi} />
+          {SCREENS.map(screen => (
+            getDrawerScreen(screen)
+          ))}
         </Drawer.Navigator>
       </Background>
     </NavigationContainer>
+  )
+}
+
+function getDrawerScreen(props) {
+  return (
+    <Drawer.Screen
+      {...props}
+      key={props.name}
+      options={{
+        ...props.options,
+        drawerIcon: (drawerIconProps) => <SidebarIcon {...drawerIconProps} source={props.icon} />
+      }}
+    />
+  )
+}
+
+function SidebarIcon(props) {
+  return (
+    <Thumbnail
+      small
+      square
+      {...props}
+      source={props.source}
+    />
   )
 }
