72759200b1557031db869abeddaf992adc996914
Brian Reynolds <brian@growratio.com>
2020-04-02 12:53:00 -0400

    back button on webview handled by app if webview can't go back

--- src/components/ui/WebViewWrapper.tsx
+++ src/components/ui/WebViewWrapper.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useRef } from 'react';
+import React, { useEffect, useRef, useState } from 'react';
 import { BackHandler } from 'react-native';
 import WebView from 'react-native-webview';
 
@@ -21,12 +21,31 @@ function useCombinedRefs(...refs) {
 }
 
 const WebViewWrapper = React.forwardRef((webviewProps, ref): JSX.Element => {
+  // webviewRef is the internal ref used by this component to handle
+  // applying internal webview navigation commands
+  // combinedRef is the internal webviewRef combined with any passed ref that
+  // needs to be forwarded to the webview component
   const webviewRef = useRef<WebView>(null);
   const combinedRef = useCombinedRefs(ref, webviewRef);
 
+  const [canGoBack, setCanGoBack] = useState(false);
+
+  const onNavigationStateChange = (navState) => {
+    webviewProps.onNavigationStateChange?.(navState);
+    if (navState) {
+      setCanGoBack(navState.canGoBack)
+    }
+  }
+
+  const onMessage = (event) => {
+    webviewProps.onMessage?.(event);
+    if (event?.nativeEvent) {
+      setCanGoBack(event.nativeEvent.canGoBack);
+    }
+  }
 
   const onAndroidBackPress = (): boolean => {
-    if (combinedRef.current) {
+    if (combinedRef.current && canGoBack) {
       combinedRef.current.goBack();
       return true; // prevent default behavior (exit app)
     }
@@ -38,14 +57,35 @@ const WebViewWrapper = React.forwardRef((webviewProps, ref): JSX.Element => {
     return (): void => {
       BackHandler.removeEventListener('hardwareBackPress', onAndroidBackPress);
     };
-  }, []); // Never re-run this effect
+  }, [canGoBack]); // update the event listener handler when canGoBack changes
 
   return (
     <WebView
       {...webviewProps}
+      injectedJavaScript={PUSH_STATE_NAVIGATION_LISTENER}
+      onNavigationStateChange={onNavigationStateChange}
+      onMessage={onMessage}
       ref={combinedRef}
     />
   )
 })
+
+const PUSH_STATE_NAVIGATION_LISTENER = `
+(function() {
+  function wrap(fn) {
+    return function wrapper() {
+      var res = fn.apply(this, arguments);
+      window.ReactNativeWebView.postMessage(window.location.href);
+      return res;
+    }
+  }
+  history.pushState = wrap(history.pushState);
+  history.replaceState = wrap(history.replaceState);
+  window.addEventListener('popstate', function() {
+    window.ReactNativeWebView.postMessage(window.location.href);
+  });
+})();
+true;
+`
 
 export default WebViewWrapper;
